fdc3c820f8ba4d5268ed2cd97e81517a3274fff1
Yuki Yoshida <y8yoshida@gmail.com>
2011-08-17 10:55:15 +0200

    Edited js/placelist.js via GitHub

--- js/placelist.js
+++ js/placelist.js
@@ -2,13 +2,13 @@ var placeObjs = [
 	chicago ={
 			name: "chicago",
 			officialName: "Gerry Rafferty - Baker Street",
-			place: "ohareairport",
+			place: "ohareairport", //facebook's place id
 //			place: "153350041353078",
-			video: "SSd0pJSKzNY",
-			checkins: "loading...",
-			interval: null,
-			startTime: 10,
-			endTime: 20
+			video: "SSd0pJSKzNY", //youtube's video id
+			checkins: "loading...", // "loading..." as initial value
+			interval: null, //null as initial value
+			startTime: 10, //what seconds this video starts at
+			endTime: 20 //what seconds this video ends at
 	},
 	heathrow ={
 			name: "heathrow",
